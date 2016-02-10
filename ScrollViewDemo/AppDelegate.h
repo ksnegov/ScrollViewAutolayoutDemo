6344e55128349285431a4de35bfdb0cfd2826811
Konstantin Snegov <ksnegov@ozon.ru>
2016-02-10 18:07:47 +0300

    Initial Commit

---
+++ ScrollViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScrollViewDemo
+//
+//  Created by Konstantin Snegov on 10/02/16.
+//  Copyright © 2016 Konstantin Snegov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ScrollViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ScrollViewDemo
+//
+//  Created by Konstantin Snegov on 10/02/16.
+//  Copyright © 2016 Konstantin Snegov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
